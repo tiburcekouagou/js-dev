c6a36bbc51f76ca60249f4df4ba659562e6b9b4f
Rene assigname <alidassigname@gmail.com>
2023-06-21 12:07:33 +0200

    dilahope

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,31 @@
 'use strict'
+document.addEventListener("DOMContentLoaded",function(){
+
+    let bouttonStart = document.getElementById("start");
+    let bouttonStop = document.getElementById("stop");
+    let bouttonReset = document.getElementById("Reset");
+    let counter = 0 ;
+    
+    bouttonStart.addEventListener("click", function () {
+            intervalId = setInterval(function () {
+                counter++;
+                console.log(counter);
+            
+        console.log(bouttonStart);
+    });
+
+    bouttonStop.addEventListener("click", function () {
+
+        console.log(bouttonStop);
+    });
+
+    bouttonReset.addEventListener("click", function () {
+        console.log(bouttonReset);
+        clearTimeout ;
+        // counter = 0 ;
+    });
+
+     ;
+
+ 
+});
