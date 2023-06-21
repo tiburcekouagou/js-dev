9d8a2f050df492c7b6eb1519354c5ba48fca66b5
Gabriel <gabrieltohouenou@gmail.com>
2023-06-21 11:09:10 +0100

    gab

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,18 @@
 'use strict'
+document.addEventListener("DOMContentLoaded", function () {
+    const chrono = document.getElementById('chrono');
+    const start = document.getElementById("start");
+    const stop = document.getElementById("stop");
+    const restart = document.getElementById("restart");
+    let counter = 0
+    
+    start.addEventListener("click", function () {
+        setTimeout(() => {
+            counter++;
+            chrono.innerHTML = counter;
+        }, 1000);
+    })
+    
+    
+
+})
