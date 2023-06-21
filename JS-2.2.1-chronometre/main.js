845a7cb7e882111519d793a3f39fa59c5d10a3a9
Possidius Capo <possidiusc@gmail.com>
2023-06-21 10:47:45 +0100

    Dossier Possidius

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,39 @@
 'use strict'
+'use strict'
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonStart = document.getElementById("start");
+    // const buttonStop = document.getElementById("stop");
+    const button_reset = document.getElementById("reset");
+    let sentance = document.querySelector("chrono")
+
+
+    let minute = 0;
+    let comptMinute;
+
+    let scde = 0;
+    let comptScde
+
+    let mllscde = 0;
+    let comptMillscde
+
+
+
+    buttonStart.addEventListener("click", function () {
+
+
+        comptMillscde = setInterval(() => {
+            mllscde++
+            if (mllscde >= 10) {
+                mllscde = 0;
+                scde += 1;
+            }
+            if (scde >= 60) {
+                scde = 0; 
+                minute += 1;
+            }
+            if (minute >= 60) { minute = 0; }
+            chrono.innerText = minute + " : " + scde + " : " + mllscde;
+        }, 100);
+        // chrono.innerText = comptMillscde
+    })
+});
