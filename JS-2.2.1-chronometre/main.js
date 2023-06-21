f770f19e3a83a90dd099c363b369583707ec0ee1
christ-oderic <christyassinguezo@gmail.com>
2023-06-21 10:45:30 +0100

    oderic push

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,12 @@
 'use strict'
+document.addEventListener("DOMContentLoaded", function(){
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
+    let mSecond = document.getElementById("m-second");
+    const start = document.getElementById("start")
+    const stop = document.getElementById("stop")
+    const reset = document.getElementById("reset")
+    start.addEventListener("click", function(){
+        
+    });
+});
