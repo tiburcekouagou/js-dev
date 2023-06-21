e5c800822c546b47a05313ebab7c50b7eea912ec
Yesfaris <yesfaris4524@gmail.com>
2023-06-21 11:01:59 +0100

    exercice

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,53 @@
 'use strict'
+document.addEventListener("DOMContentLoaded", function () {
+
+let counter = 0;
+let end;
+let stop
+let tie = 0;
+let sec = 0;
+let min = 0;
+let heu = 0;
+let dec = 0;
+
+    const button1 = document.getElementById("debut");
+    const button2 = document.getElementById("arret");
+    const button3 = document.getElementById("retour");
+    const chrono = document.getElementById("chrono")
+    console.log(button1, button2, button3);
+
+    chrono.innerText = heu + ' : ' + min + ' :' + sec ;
+
+    let start = document.getElementById("debut")
+    button1.addEventListener("click",
+        function timer(){
+            intervalId = setInterval(function(){
+        
+                    
+                chrono.innerText = heu + ' : ' + min + ' : ' + sec;
+                dec++ ;
+                if(dec >= 10){
+                    dec = 0; sec++ ;
+                }
+                if(sec >= 60){
+                    sec = 0; min++ ;
+                }
+                if(min >= 60){
+                    min = 0; heu++;
+                }
+            }, 1)
+        })
+        let Stop= document.getElementById("arret");
+    button2.addEventListener("click", function(){
+      clearInterval(end)
+     });
+     
+
+
+     let reset= document.getElementById("retour");
+     button3.addEventListener("click", function(){
+        chrono.innerText = heu + ' : ' + min + ' : ' + sec;
+      });
+ 
+});
+
