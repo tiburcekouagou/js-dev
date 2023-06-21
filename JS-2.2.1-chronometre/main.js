dd53d7c70afe7181dfde300fbc2b05474c82b59c
Ahoumenou Emile <ahoumenouemile022@gmail.com>
2023-06-21 10:53:12 +0100

    chrono

--- JS-2.2.1-chronometre/main.js
+++ JS-2.2.1-chronometre/main.js
@@ -1,1 +1,28 @@
 'use strict'
+'use strict'
+
+let tierces = document.querySelector(".tierces")
+let secondes = document.querySelector(".secondes")
+let times = document.querySelector(".times")
+
+ document.getElementById("chrono").innerHTML = times.toString().padStart(2, "0") + ":" +
+                                               secondes.toString().padStart(2, "0") + ":" +
+                                               tierces.toString().padStart(2, "0")
+                                               
+
+
+function fonctionchro() {
+    tierces = 0;
+    secondes = 0;
+    times = 0;
+
+    if (tierces <= 100) {
+        tierces++
+        if (secondes >= 60) {
+            secondes++
+        }
+    }
+   
+}
+
+console.log(fonctionchro());
