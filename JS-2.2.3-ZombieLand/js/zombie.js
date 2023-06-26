7b09de725ccd50379af83c1deba08a43e1095710
Euloge <eagnoun@gmail.com>
2023-06-26 18:12:12 +0100

    update

--- JS-2.2.3-ZombieLand/js/zombie.js
+++ JS-2.2.3-ZombieLand/js/zombie.js
@@ -1,62 +1,115 @@
 'use strict';
 
+// document.addEventListener("DOMContentLoaded", () => {
+//     let zombie = document.querySelector("#zombietomb");
+//     zombie.innerHTML = '<img src="./img/zombie.png" id="zombieT">';
+//     let momie = document.querySelector("#zombieT");
+//     let move = 0;
+//     let zombieWidth = zombie.offsetWidth;
+//     console.log(zombieWidth);
+//     let zombieMomie = momie.offsetWidth;
+//     console.log(zombieMomie);
+//     let timeout;
+//     let posRight = 1;
+//     let directRight = 0 ;
+//     let directY = 0 ;
+
+
+//     function displayZombie() {
+//         move -= zombieWidth;
+//         zombie.style.overflow = "hidden";
+
+//         if(move <= -zombieMomie+100) {
+//             move = -zombieMomie+100;
+//             clearTimeout(timeout);
+//         };
+
+
+//         timeout = setTimeout(() => {
+//             momie.style.transform = "translateX(" + (move) + "px)";
+//             displayZombie();
+//         }, 200);
+
+
+//         document.addEventListener("keydown", function(event) {
+
+//             console.log(event.key);
+//             if (event.key === "ArrowRight") {
+//                 directRight -= posRight ;
+//                 zombie.style.transform = "rotateY(180deg) translateX(" + (directRight) + "px)";
+//             }
+//             if(event.key === "ArrowLeft"){
+//                 posRight = -1 ;
+//                 zombie.style.transform = "rotateY(0deg) translateX(" + (directRight) + "px)";
+//             }
+
+//             if (event.key === "ArrowDown") {
+//                 directY++
+//                 zombie.style.transform = "translateY(" + (directY) + "px)";
+//             }else if(event.key === "ArrowUp"){
+//                 directY -=1
+//                 zombie.style.transform = "translateY(" + (directY) + "px)";
+//             }
+
+
+
+//         });
+//     }
+
+//     displayZombie();
+
+
+
+
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
     let zombie = document.querySelector("#zombietomb");
-    zombie.innerHTML = '<img src="./img/zombie.png" id="zombieT">';
-    let momie = document.querySelector("#zombieT");
-    let move = 0;
-    let zombieWidth = zombie.offsetWidth;
-    console.log(zombieWidth);
-    let zombieMomie = momie.offsetWidth;
-    console.log(zombieMomie);
-    let timeout;
-    let direct = 0 ;
-    let directY = 0 ;
-
-
-    function displayZombie() {
-        move -= zombieWidth;
-        zombie.style.overflow = "hidden";
-
-        if(move <= -zombieMomie+100) {
-            move = -zombieMomie+100;
-            clearTimeout(timeout);
-        };
-        
-
-        timeout = setTimeout(() => {
-            momie.style.transform = "translateX(" + (move) + "px)";
-            displayZombie();
+    let position = 0;
+    let divWidth = zombie.scrollWidth;
+    let imageWidth = divWidth * 11;
+    let arrowSens = 1;
+    let divSpeed = 100;
+    let divPos = 0;
+    residentEvil();
+    function residentEvil() {
+        position -= divWidth;
+        zombie.style.backgroundPosition = `${position}px`;
+        const timeout = setTimeout(() => {
+            requestAnimationFrame(residentEvil);
         }, 200);
 
-
-        document.addEventListener("keydown", function (event) {
-            
-            console.log(event.key);
-            if (event.key === "ArrowRight") {
-                direct++
-                zombie.style.transform = "translateX(" + (direct) + "px)";
-            }else if(event.key === "ArrowLeft"){
-                direct -=1
-                zombie.style.transform = "translateX(" + (direct) + "px)";
-            }
-
-            if (event.key === "ArrowDown") {
-                directY++
-                zombie.style.transform = "translateY(" + (directY) + "px)";
-            }else if(event.key === "ArrowUp"){
-                directY -=1
-                zombie.style.transform = "translateY(" + (directY) + "px)";
-            }
-
-    
-            
-        });
+        if (position === -imageWidth + divWidth) {
+            clearTimeout(timeout);
+        }
     }
 
-    displayZombie();
+    document.addEventListener("keydown", function (event) {
+        console.log(event.key);
+        divPos += arrowSens * divSpeed;
+        if (event.key === "ArrowRight") {
+            arrowSens = 1
+            zombie.style.transform = "rotateY(180deg) translate(" + (-divPos) + "px)";
+        } else if (event.key === "ArrowLeft") {
+            arrowSens = -1;
+            zombie.style.transform = "rotateY(0deg) translate(" + (divPos) + "px)";
+        }
+        if(divPos >= (window.innerWidth - divWidth)){
+            zombie.style.transform = "translate(" + (window.innerWidth - divWidth) + "px)" ;
+        }
+        if(divPos <= 0){
+            zombie.style.transform = "translate(" + (0) + "px)" ;
+        }
 
-  
+        if (event.key === "ArrowDown") {
+            arrowSens = 1
+            zombie.style.transform = "translateY(" + (divPos) + "px)";
+        } else if (event.key === "ArrowUp") {
+            arrowSens = -1
+            zombie.style.transform = "translateY(" + (divPos) + "px)";
+        }
 
 
+    });
 });
--- JS-2.2.4-Rocket/js/main.js
+++ JS-2.2.4-Rocket/js/main.js
@@ -1,17 +1,70 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", ()=>{
 
 /***********************************************************************************/
 /* *********************************** DONNEES *************************************/
 /***********************************************************************************/
-
+    let billboard = document.querySelector("#billboard span");
+    let count = 11;
+    let click = 0;
+    let rocket = document.querySelector("#rocket") ;
+    let btn = document.querySelector("#firing-button");
+    btn.disabled = true;
+    let btnCancel = document.querySelector("#cancel-button");
+    let star = document.querySelector(".star")
 
 
 /***********************************************************************************/
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
+    /*================== La fonction du minuteur... ===================*/
+
+    function chrono () {
+        
+        count--;
+        
+
+        billboard.innerText = `${count}`;
+        if (count > 0 && count <= 10) {
+            rocket.src = "images/rocket2.gif"
+        }
+
+        
+        const timeout = setTimeout(chrono, 1000);
+        
+        if (count === 0) {
+            count = 0 ;
+            rocket.src = "images/rocket3.gif"
+            rocket.classList.add("tookOff");
+            clearTimeout(timeout);
+        }
+        
+    }
+
+    /*======================= Start button ==========================*/
+
+
+    function button (){
+        btn.addEventListener("click", ()=>{ 
+            btn.classList.add("disabled");
+            chrono();
+            
+        });
+    }
+
+
+    /*======================== Stars ============================*/
+
+    function generateStars(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min) ;
+    }
 
 
 
 /************************************************************************************/
 /* ******************************** CODE PRINCIPAL **********************************/
 /************************************************************************************/
+    star
+   button();
+
+});
